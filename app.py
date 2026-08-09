607b124c384050266c88308603455fd5d14e012f
SeemaTVS <seematvs9@gmail.com>
2026-08-09 12:07:06 +0530

    Update Google Sheets URL format for data loading

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ st.title("TVS Agency Inventory & Order Management")
 import re
 def load_data():
     try:
-        sheet_url = "https://docs.google.com/spreadsheets/d/1R10l4CrYCS2I-lGDe-90zp4bZa5Ug_d64vWb5u9Ns/export?format=csv&gid=0"
+      sheet_url = "https://docs.google.com/spreadsheets/d/1R10l4CrYCS2I-lGDe-90zp4bZa5Ug_d64vWb5u9Ns/gviz/tq?tqx=out:csv"
         df_loaded = pd.read_csv(sheet_url)
         if 'units_sold' not in df_loaded.columns:
             df_loaded['units_sold'] = 0
